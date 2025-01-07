3ee076a488b22c7cd3c39ca7b98982f6b7771cc1
NatashaSolntseva <84683371+NatashaSolntseva@users.noreply.github.com>
2025-01-07 15:28:20 +0300

    fix: photo link

--- src/constants/profile/socials.ts
+++ src/constants/profile/socials.ts
@@ -1,24 +1,24 @@
 export const SOCIALS = [
   {
-    icon: '/src/assets/icons/instagram.svg',
+    icon: '@/assets/icons/instagram.svg',
     label: 'Instagram',
     value: '@anurag.yadav.007',
     link: 'https://instagram.com/anurag.yadav.007'
   },
   {
-    icon: '/src/assets/icons/git.svg',
+    icon: '@/assets/icons/git.svg',
     label: 'GitHub',
     value: '@anuragyadav365',
     link: 'https://github.com/anuragyadav365'
   },
   {
-    icon: '/src/assets/icons/twitter.svg',
+    icon: '@/assets/icons/twitter.svg',
     label: 'Twitter',
     value: '@anuramnp',
     link: 'https://twitter.com/anuramnp'
   },
   {
-    icon: '/src/assets/icons/linkedin.svg',
+    icon: '@/assets/icons/linkedin.svg',
     label: 'LinkedIn',
     value: '@anuragyadav',
     link: 'https://linkedin.com/in/anuragyadav'
