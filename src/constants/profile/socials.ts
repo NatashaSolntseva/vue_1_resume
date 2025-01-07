f27d3f48c24205339e0a2f21e30ba6a19c760024
NatashaSolntseva <84683371+NatashaSolntseva@users.noreply.github.com>
2025-01-07 15:35:58 +0300

    fix: photo link

--- src/constants/profile/socials.ts
+++ src/constants/profile/socials.ts
@@ -1,24 +1,29 @@
+import instagramIcon from '@/assets/icons/instagram.svg'
+import gitIcon from '@/assets/icons/git.svg'
+import twitterIcon from '@/assets/icons/twitter.svg'
+import linkedinIcon from '@/assets/icons/linkedin.svg'
+
 export const SOCIALS = [
   {
-    icon: '@/assets/icons/instagram.svg',
+    icon: instagramIcon,
     label: 'Instagram',
     value: '@anurag.yadav.007',
     link: 'https://instagram.com/anurag.yadav.007'
   },
   {
-    icon: '@/assets/icons/git.svg',
+    icon: gitIcon,
     label: 'GitHub',
     value: '@anuragyadav365',
     link: 'https://github.com/anuragyadav365'
   },
   {
-    icon: '@/assets/icons/twitter.svg',
+    icon: twitterIcon,
     label: 'Twitter',
     value: '@anuramnp',
     link: 'https://twitter.com/anuramnp'
   },
   {
-    icon: '@/assets/icons/linkedin.svg',
+    icon: linkedinIcon,
     label: 'LinkedIn',
     value: '@anuragyadav',
     link: 'https://linkedin.com/in/anuragyadav'
